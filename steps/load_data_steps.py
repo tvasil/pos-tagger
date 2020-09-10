07f508dd7df9b6bfdba47f593036b85f840794dc
Tania <tvasilikioti@babbel.com>
2020-09-10 23:36:30 +0200

    add data loading steps for train and predict

---
+++ steps/load_data_steps.py
@@ -0,0 +1,22 @@
+from typing import Generator
+from pipeline.pipeline import Step
+from helpers import read_conllu_file, read_txt_file
+
+
+class LoadConlluDataStep(Step):
+
+    def __init__(self, filenames: list, batch_size: int):
+        self.filenames = filenames
+        self.batch_size = batch_size
+
+    def run(self, batches: Generator):
+        return read_conllu_file(self.filenames, self.batch_size)
+
+
+class LoadTxtDataStep(Step):
+
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def run(self, batches: Generator):
+        return read_txt_file(self.filename)
