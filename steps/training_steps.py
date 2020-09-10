a072a5abe88f397c4b84f58888a4fef547ee9243
Tania <tvasilikioti@babbel.com>
2020-09-10 23:48:44 +0200

    add training steps

---
+++ steps/training_steps.py
@@ -0,0 +1,33 @@
+import time
+from sklearn_crfsuite import CRF
+from os import path
+from typing import Generator
+
+from pipeline.pipeline import Step
+
+
+class CRFTrainStep(Step):
+    def __init__(self, model_file_path):
+        # load the model if it exists
+        self.model_file_path = path.abspath(path.expanduser(model_file_path))
+        self.model = CRF(
+                algorithm='l2sgd',
+                c2=0.1,
+                max_iterations=1000,
+                all_possible_transitions=True,
+                model_filename=self.model_file_path)
+
+    def run(self, batches: Generator):
+        st = time.time()
+
+        x = []
+        y = []
+        for batch in batches:
+            b = list(batch)
+            x.extend(b[0])
+            y.extend(b[1])
+
+        self.model = self.model.fit(x, y)
+
+        et = time.time()
+        print(f"The model finished training in {round(et-st, 2)} seconds.")
