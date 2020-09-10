c3720a1d983840c1ab0f2f232d9a821b19c7dd91
Tania <tvasilikioti@babbel.com>
2020-09-10 23:48:29 +0200

    add evaluation steps

---
+++ steps/evaluation_steps.py
@@ -0,0 +1,46 @@
+import time
+from sklearn_crfsuite import CRF
+from sklearn_crfsuite import metrics
+
+from typing import Generator
+
+from os import path
+from pipeline.pipeline import Step
+
+
+class CRFEvaluateStep(Step):
+    def __init__(self, model_file_path):
+        self.model_file_path = path.abspath(path.expanduser(model_file_path))
+        self.model = CRF(
+                algorithm='l2sgd',
+                c2=0.1,
+                max_iterations=1000,
+                all_possible_transitions=True,
+                model_filename=self.model_file_path)
+
+    def run(self, batches: Generator):
+        st = time.time()
+
+        x = []
+        y = []
+        for batch in batches:
+            b = list(batch)
+            x.extend(b[0])
+            y.extend(b[1])
+
+        score = self.model.score(x, y)
+        y_pred = self.model.predict(x)
+        score_sentence = metrics.sequence_accuracy_score(y, y_pred)
+        classification_report = metrics.flat_classification_report(
+            y,
+            y_pred,
+            labels=self.model.classes_)
+        print("*"*80)
+        print("F1 score on Test Data")
+        print(round(score, 3))
+        print("Sequence accurancy score (% of sentences scored 100% correctly):")
+        print(round(score_sentence, 3))
+        print("Class-wise classification report:")
+        print(classification_report)
+        et = time.time()
+        print(f"The model finished evaluating in {round(et-st, 2)} seconds.")
